af15cb72a921f8b614e84451ba5af00c9aaee61d
Astatsu <vinijao123690@gmail.com>
2022-12-26 07:15:30 -0300

    Mudanças: -Componentes para cada mês -Mês mostrado de acordo com seleção

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <seletor @selecionar="seletor=$event" />
+    <janeiro v-if="seletor==1" />
+    <fevereiro v-if="seletor==2" />
+    <março v-if="seletor==3"/>
+    <abril v-if="seletor==4"/>
+    <maio v-if="seletor==5"/>
+    <junho v-if="seletor==6"/>
+    <julho v-if="seletor==7"/>
+    <agosto v-if="seletor==8"/>
+    <setembro v-if="seletor==9"/>
+    <outubro v-if="seletor==10"/>
+    <novembro v-if="seletor==11"/>
+    <dezembro v-if="seletor==12"/>
+    {{ seletor }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import seletor from './components/seletor.vue'
+import janeiro from './components/janeiro.vue'
+import fevereiro from './components/fevereiro.vue'
+import março from './components/março.vue'
+import abril from './components/abril.vue'
+import maio from './components/maio.vue'
+import junho from './components/junho.vue'
+import julho from './components/julho.vue'
+import agosto from './components/agosto.vue'
+import setembro from './components/setembro.vue'
+import outubro from './components/outubro.vue'
+import novembro from './components/novembro.vue'
+import dezembro from './components/dezembro.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    janeiro,
+    seletor,
+    fevereiro,
+    março,
+    abril,
+    maio,
+    junho,
+    julho,
+    agosto,
+    setembro,
+    outubro,
+    novembro,
+    dezembro,
+  },
+  data() {
+    return {
+      seletor: 1
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/dezembro.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <h1>Dezembro</h1>
+        <table class="">
+            <tr class="row">
+                <th class="tst">Data</th>
+                <th>Descrição</th>
+                <th>Tipo</th>
+                <th>Quantidade</th>
+            </tr>
+            <tr>
+                <td>
+                    <input type="date" id="data">
+                </td>
+                <td>
+                    <input type="text" class="desc">
+                </td>
+                <td>
+                    <select name="" id="">
+                        <option value="0">Gasto</option>
+                        <option value="1">Ganho</option>
+                    </select>
+                </td>
+                <td class="din">
+                    R$
+                    <input type="number">
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'dezembro',
+}
+
+</script>
+
+<style>
+
+table {
+    width: 500px;
+    border: 1px solid;
+}
+
+th{
+    border: 1px solid black;
+    text-align: center;
+}
+.din {
+    display: flex;
+}
+
+</style>
