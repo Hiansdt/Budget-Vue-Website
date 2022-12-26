d5d0ee70ab69d36dd1e37d813fcd9f4418d1a09e
Astatsu <vinijao123690@gmail.com>
2022-12-26 15:15:04 -0300

    Mudanças:
    -Header
    -Função para somar valores

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,53 @@
 <template>
   <div>
+    <Header :totalSobra="state.total" />
   </div>
 </template>
 
 <script>
-import seletor from './components/seletor.vue'
-import tracker from './components/tracker.vue'
+import Header from './components/header.vue'
+import {reactive, computed} from 'vue'
 
 export default {
   name: 'App',
   components: {
-    seletor,
-    tracker,
+    Header,
   },
+  setup() {
+
+    const state = reactive ({
+      ganho: [],
+      total: computed(()=> {
+        let temp = 0;
+
+        if (state.ganho.length > 0) {
+          for (let i = 0; i < state.ganho.length; i++) {
+            temp += state.ganho[i].value
+          }
+        }
+
+        return temp
+      })
+    })
+
+    return {
+      Header,
+      state
+    }
+  }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
+
+body {
+  background: #EEE;
+}
+
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <h1 class="title">Budget-Tracker</h1>
+        <div class="total-sobra">
+            <h2>Total Gasto</h2>
+            <p>R${{ totalSobra }}</p>
+        </div>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'Header',
+    props: {
+        totalSobra: Number,
+    }
+}
+
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        background-color: rgb(48, 48, 48);
+        color: aliceblue;
+        height: 100px;
+        align-items: center;
+        justify-content: space-between;
+        padding: 15px 30px;
+        border-bottom: 10px solid rgb(255, 0, 157) ;
+    }
+    .total-sobra > p{
+        background-color: rgb(255, 0, 157);
+        font-weight: bold;
+        font-size: large;
+        text-align: center;
+        border-radius: 5px;
+    }
+</style>
